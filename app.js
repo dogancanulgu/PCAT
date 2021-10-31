36cd0821b7d930c3e1f4593136e066cc2cee31c6
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-10-31 23:46:10 +0300

    Model-view

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-// const ejs = require('ejs');
-// const path = require('path');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -13,18 +20,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { photos: photos });
 });
+
+app.get('/photos/:id', async (req, res) => {
+  // console.log(req.params.id);
+  // res.render('about');
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
 app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
   res.redirect('/');
 });
 
